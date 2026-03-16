92dada37890d7c455649327bff18eff16680db0f
juniorsilvacc <juniorsilvacc@hotmail.com>
2026-03-16 13:53:12 -0300

    feat: implementação do docker na aplicação

--- nivel-02/src/database.py
+++ nivel-02/src/database.py
@@ -1,13 +1,36 @@
+import os
+import time
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.engine import URL
+
+load_dotenv()
 
 def get_engine():
     url = URL.create(
         drivername="postgresql+psycopg2",
-        username="postgres",
-        password="root",
-        host="localhost",
-        port="5432",
-        database="modern-data-etl-nv1"
+        username=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASS"),
+        host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT"),
+        database=os.getenv("DB_NAME")
     )
-    return create_engine(url)
+    
+    engine = create_engine(url)
+    
+    max_retries = 5
+    retry_interval = 5
+
+    print(f"📡 Tentando conectar ao banco em {os.getenv('DB_HOST')}...")
+
+    for i in range(max_retries):
+        try:
+            with engine.connect() as conn:
+                conn.execute()
+            print("✅ Banco de dados pronto para receber conexões!")
+            return engine
+        except Exception as e:
+            print(f"⏳ Banco ainda não disponível (Tentativa {i+1}/{max_retries}). Aguardando {retry_interval}s...")
+            time.sleep(retry_interval)
+            
+    raise Exception("❌ Erro: Não foi possível conectar ao banco de dados após várias tentativas.")
